fa474b1e9d7c337147fa85665975c68ad5f09c3b
fredo <luehrs.fred@gmail.com>
2024-04-12 21:42:40 +0200

    scripts: deploy script for munus

--- script/DeployMunus.s.sol
+++ script/DeployMunus.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import "../src/Munus.sol";
 
 contract CounterScript is Script {
+
+    public Munus munus;
+
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        munus = new Munus();
+        vm.stopBroadcast();
     }
 }
