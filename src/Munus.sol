ac761ba6a7f266638847f2dbc9c38e0daf9fb35e
fredo <luehrs.fred@gmail.com>
2024-04-12 23:11:25 +0200

    contracts: add DonationReceived event

--- src/Munus.sol
+++ src/Munus.sol
@@ -7,6 +7,8 @@ contract Munus {
     error SecretAlreadyUsed();
     error TransferFailed();
 
+    event DonationReceived(address receiver, bytes32 hash);
+
     function trampoline(bytes32 hash, address payable payout) public payable {
         // A hash can be used only once. This assumes hash collision resistance.
         if (secrets[hash]) { revert SecretAlreadyUsed(); }
@@ -16,5 +18,7 @@ contract Munus {
         (bool result, ) = payout.call{value: msg.value}("");
 
         if (!result) { revert TransferFailed(); }
+
+        emit DonationReceived(payout, hash);
     }
 }
